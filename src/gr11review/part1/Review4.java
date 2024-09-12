1dc9364466db8e2fe41e05230151dc5a60dd226c
Gordon <gordon.zhu25@ycdsbk>
2024-09-12 19:18:14 -0400

    WIP Review5

--- src/gr11review/part1/Review4.java
+++ src/gr11review/part1/Review4.java
@@ -6,8 +6,9 @@ import java.lang.Math.*;
 import java.text.DecimalFormat;
 
 /**
- * Create a program that asks the user to enter a month number and day and then
- * prints the day of the year
+ * Asks the user how many items he/she would like to purchase. The program then
+ * gets the prices for the items one by one. At the end of entering items, print
+ * out the total, the tax (13%) and the grand total.
  * 
  * @author Gordon Z
  */
@@ -33,9 +34,9 @@ public class Review4 {
 
         // Getting the items
         for (int i = 0; i < intItems; i++) {
-            out.print("Enter the price for item " + (i+1) + ": ");
+            out.print("Enter the price for item " + (i + 1) + ": ");
             out.flush();
-            //adding new price to subtotal
+            // adding new price to subtotal
             dblSubTotal += Double.parseDouble(br.readLine());
         }
 
--- src/gr11review/part1/Review5.java
+++ src/gr11review/part1/Review5.java
@@ -1,1 +1,30 @@
 package gr11review.part1;
+
+import java.util.*;
+import java.io.*;
+import java.lang.Math.*;
+import java.text.DecimalFormat;
+
+/**
+ * Write a program that prompts for a yearly_amount, annual
+ * compound_interest_rate, and outputs the number of years .
+ * 
+ * @author Gordon Z
+ */
+
+public class Review5 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Initialization
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+        DecimalFormat df = new DecimalFormat("0.00");
+
+        
+
+        out.flush();
+        br.close();
+        out.close();
+    }
+}
