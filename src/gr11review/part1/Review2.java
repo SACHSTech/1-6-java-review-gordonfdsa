cb81f3b796d241e70954fc4ed244d5313f859d91
Gordon <gordon.zhu25@ycdsbk>
2024-09-12 17:58:10 -0400

    Completed Review2

--- src/gr11review/part1/Review2.java
+++ src/gr11review/part1/Review2.java
@@ -24,16 +24,16 @@ public class Review2 {
         System.out.println("2 - print a joke about your clothes");
         System.out.println("3 - print a joke about your teacher");
 
-        System.out.print("choose a menu option: ");
+        System.out.print("Choose a menu option: ");
 
         // Inputs
         int intOption = Integer.parseInt(br.readLine());
 
-        // Outputs
+        // Outputs based on user selection
         switch (intOption) {
             case 0:
                 // joke about hair
-                System.out.println("I was going to make a joke about my hair\nBut it’s a bit over my head!");
+                System.out.println("I was going to make a joke about my hair\nBut its a bit over my head!");
                 break;
             case 1:
                 // joke about feet
@@ -41,13 +41,15 @@ public class Review2 {
                 break;
             case 2:
                 // joke about clothes
-                System.out.println("Why does my clothes and I have an agreement?\nI wear them, and they wrinkle in protest.");
+                System.out.println(
+                        "Why does my clothes and I have an agreement?\nI wear them, and they wrinkle in protest.");
                 break;
             case 3:
                 // joke about teacher
                 System.out.println("Why did the music teacher need a ladder?\nTo reach the high notes.");
                 break;
             default:
+                // invalid option
                 System.out.println("Invalid menu option");
                 break;
         }
